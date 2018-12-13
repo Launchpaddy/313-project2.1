cf8e94f8ae2b60639cc3528e0145e3153d1609fd
Launchpaddy <trevikp@gmail.com>
2018-12-12 20:07:14 -0700

    adding id to the session

--- public/login-clientside.js
+++ public/login-clientside.js
@@ -37,7 +37,7 @@ function verifyLogin() {
    var params = {username:username, password:password};
 
 
-   $.get("/verifyLogin", function(data) {
+   $.get("/verifyLogin",params , function(data) {
       console.log("Back from the server with : ");
       console.log(data);
 
