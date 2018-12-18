c0c9351fd0ee74ea4eda1da5e574fb67cb5a3cfb
Launchpaddy <trevikp@gmail.com>
2018-12-18 01:42:34 -0700

    finishing up stuff and now its broken

--- public/main-clientside.js
+++ public/main-clientside.js
@@ -1,5 +1,27 @@
 
 /*********************************************************
+* sends the request to get a user by id
+* when it comes back it append our page with the data
+*********************************************************/
+function searchUserById() {
+   // body...
+   console.log("searchUserById");
+   var userId = $("#userId").val();
+   console.log("userId: " + userId);
+
+   $.get("/user", { userId: userId }, function(data) {
+      console.log("Back from the server with : ");
+      console.log(data);
+
+      for (var i = 0; i < data.length ; i++) {
+         var users = data[i];
+         $("#ulUsers").append("<li>" + users.id + " " + users.display_name +"</li>");
+         console.log(users);
+      }
+   });
+}
+
+/*********************************************************
 * when we click on a sport we ant to go through all of its 
 * 
 *********************************************************/
